42c5a1077bc4c90a1a9dbef8924e184f962e7850
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-03-04 19:02:23 +0800

    feat: init

---
+++ routes/index.ts
@@ -0,0 +1,18 @@
+export default eventHandler((evt) => {
+  const host = evt.node.req.headers.host
+  switch (host) {
+    case 'channel.sxzz.moe':
+      sendRedirect(evt, 'https://t.me/+6221NdOWC5hiYzU1')
+      return
+    case 'chat.sxzz.moe':
+      sendRedirect(evt, 'https://discord.com/invite/2bHNPmTsy4')
+      return
+    case 'tg-group.sxzz.moe':
+      sendRedirect(evt, 'https://t.me/sxzz_group')
+      return
+    case 'sxzz.moe':
+      sendRedirect(evt, 'https://blog.sxzz.moe')
+      return
+  }
+  return host
+})
