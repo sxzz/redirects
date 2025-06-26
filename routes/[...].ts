c717c15166f7d5b8ce5830db733268478bc4e3e7
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-06-27 03:09:49 +0800

    refactor: cleanup

--- routes/[...].ts
+++ routes/[...].ts
@@ -2,24 +2,21 @@ export default eventHandler((evt) => {
   const host = evt.node.req.headers.host
   switch (host) {
     case 'channel.sxzz.moe':
-      // sendRedirect(evt, 'https://t.me/sxzz_channel')
-      return 'No longer public.'
-    case 'chat.sxzz.moe':
-      sendRedirect(evt, 'https://discord.gg/r3knZmVCxf')
-      return
     case 'tg-group.sxzz.moe':
+      // sendRedirect(evt, 'https://t.me/sxzz_channel')
       // sendRedirect(evt, 'https://t.me/+SfNJUvZ8YBszMGVl')
       return 'No longer public.'
+    case 'chat.sxzz.moe':
+      return sendRedirect(evt, 'https://discord.gg/r3knZmVCxf')
+
     case 'sxzz.moe':
     case 'sxzz.dev':
     case 'www.sxzz.moe':
     case 'www.sxzz.dev':
     case 'blog.sxzz.moe':
-      sendRedirect(evt, 'https://xlog.sxzz.moe')
-      return
+      return sendRedirect(evt, 'https://xlog.sxzz.moe')
     case 'ama.sxzz.moe':
-      sendRedirect(evt, 'https://xlog.sxzz.moe/chat')
-      return
+      return sendRedirect(evt, 'https://xlog.sxzz.moe/chat')
   }
   return host
 })
