daada9f4b70fa7dd610690b9c625f1d0042dbc95
Kevin Deng <sxzz@sxzz.moe>
2026-01-24 11:08:24 +0800

    feat: improve article page

--- server.ts
+++ server.ts
@@ -32,43 +32,60 @@ export default eventHandler((evt) => {
       if (redirectUrl) {
         return redirect(redirectUrl)
       }
-      return html`
-        <body
-          style="font-family: monospace; line-height: 1.6; font-size: 16px;"
-        >
-          <h1>Articles</h1>
-          <ul>
-            <li>
-              <a
-                target="_blank"
-                href="https://gist.github.com/sxzz/920d4480c8b507f6fb6606421cbf7028"
-                >2022，我的开源探索</a
+      return html`<!doctype html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0"
+            />
+            <title>Kevin Deng's Articles (temporary page)</title>
+          </head>
+          <body
+            style="font-family: monospace; line-height: 1.6; font-size: 16px"
+          >
+            <h1>Articles</h1>
+            <ul>
+              <li>
+                <a
+                  target="_blank"
+                  href="https://gist.github.com/sxzz/920d4480c8b507f6fb6606421cbf7028"
+                  >2022，我的开源探索</a
+                >
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  href="https://gist.github.com/sxzz/3995fc7251567c7c95de35f45539b9c2"
+                  >Vue 3.3 主要新特性详解</a
+                >
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  href="https://gist.github.com/sxzz/a20bb470a8fde0a04115a4d2df8e8313"
+                  >HackerGame 2024 Writeup</a
+                >
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  href="https://gist.github.com/sxzz/a7c0d454b012bdca94e386a9520490be"
+                  >踩坑异闻录——Windows 前端工具链之痛</a
+                >
+              </li>
+            </ul>
+
+            <div style="font-size: 14px">
+              <a target="_blank" href="https://sxzz.dev">Homepage</a> /
+              <a target="_blank" href="https://github.com/sxzz">GitHub</a> /
+              <a target="_blank" href="https://bsky.app/profile/sxzz.dev"
+                >Bluesky</a
               >
-            </li>
-            <li>
-              <a
-                target="_blank"
-                href="https://gist.github.com/sxzz/3995fc7251567c7c95de35f45539b9c2"
-                >Vue 3.3 主要新特性详解</a
-              >
-            </li>
-            <li>
-              <a
-                target="_blank"
-                href="https://gist.github.com/sxzz/a20bb470a8fde0a04115a4d2df8e8313"
-                >HackerGame 2024 Writeup</a
-              >
-            </li>
-            <li>
-              <a
-                target="_blank"
-                href="https://gist.github.com/sxzz/a7c0d454b012bdca94e386a9520490be"
-                >踩坑异闻录——Windows 前端工具链之痛</a
-              >
-            </li>
-          </ul>
-        </body>
-      `
+            </div>
+          </body>
+        </html> `
     }
 
     case 'ama.sxzz.moe':
